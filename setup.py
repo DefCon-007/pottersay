957d5b69602e80f86ac2901d59586ea2003b7bff
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2021-07-04 16:05:34 +0530

    Update sources

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pottersay',
-    version='0.1.3',
+    version='0.1.4',
     description='Generate a random Harry Potter quote in ASCII art.',
     url='http://github.com/defcon-007/pottersay',
     author='Ayush Goyal',
